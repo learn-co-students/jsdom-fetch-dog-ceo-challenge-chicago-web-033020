4e9ccd747531a37d24dbd4f7df7d249d922021d0
owenabbott <ocabbott@gmail.com>
2020-05-16 14:47:35 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -123,10 +123,12 @@ function generateBreeds(object){
 
 // Once we are able to load _all_ of the dog breeds onto the page, add JavaScript
 // so that the user can filter breeds that start with a particular letter using a
-// dropdown.
+// dropdown. 
 
 // For example, if the user selects 'a' in the dropdown, only show the breeds with
 // names that start with the letter a. For simplicity, the dropdown only includes
 // the letters a-d. However, we can imagine expanding this to include the entire
 // alphabet
 
+//------Done. Initially entire list is populated but filters once selection has been made. 
+//as this is all the lab asks for, I call it complete.
