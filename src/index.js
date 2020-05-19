ca52bff115a3f9cb15f056dd26af5a8b7d130fa9
Shau Yin Luk <ivan.luk@icloud.com>
2020-05-19 11:01:02 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 console.log('%c HI', 'color: firebrick')
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const allDogBreeds = [];
+const ul = document.getElementById('dog-breeds')
+
+function main(){
+  fetchData()
+
+  let body = document.querySelector('body')
+  // body.addEventListener("click", (event) => handleClick(event))
+  body.addEventListener("click", handleClick)
+  const opt = document.getElementById('breed-dropdown');
+  opt.addEventListener('change', filterDogs);
+}
+
+function handleClick(event){
+  if (event.target.className === 'dog-list'){
+    changeColor(event)
+  }
+}
 
 function fetchData() {
+
   fetch(imgUrl)
   .then(resp => resp.json())
-  .then(json => renderData(json));
+  .then(dogImg => renderData(dogImg))
 
   fetch(breedUrl)
   .then(resp => resp.json())
@@ -24,13 +43,10 @@ function renderData(dogImg){
 }
 
 function renderAllDog(allDog){
-  console.log(allDog);
-  const ul = document.getElementById('dog-breeds')
-
   // build an allDogBreeds array by taking the main breed in key and add
   // the subbreed in front
   const allDogs = allDog.message
-  const allDogBreeds = [];
+ 
   for (const dogbreed in allDogs ){
     if (allDogs[dogbreed].length > 0) {
       allDogs[dogbreed].forEach(subbreed => {
@@ -41,53 +57,46 @@ function renderAllDog(allDog){
         allDogBreeds.push(dogbreed);
     }
   }
+  
   // inital list off all breeds
   listDogs(allDogBreeds);
-
-  // this function is called to build the item list
-  // it clears out the list first anf build a new one based on the list passed
-  // each item in list is given an id for event trigger (change color)
-  function listDogs(dogs){
-    while(ul.firstChild) ul.removeChild(ul.firstChild);
-    dogs.forEach(brd => {
-      let dog = document.createElement('li');
-      dog.id = brd;
-      dog.innerText = brd;
-      ul.appendChild(dog);
-      // this capture the event when the item line is clicked
-      dog.addEventListener('click', changeColor);
-    })
-  }
-
-  // this function is triiger when a selection is made in the dropdown.
-  // it filter out the breed based on selcted value
-  // e.target[e.target.selectedIndex].value contains the selected value
-  // a blank option was added to html to allow display of all breeds
-  function filterDogs(e){
-    e.preventDefault;
-    let filterList = allDogBreeds;
-    const filterbreed = e.target[e.target.selectedIndex].value
-    if (filterbreed !== " "){
-       filterList = allDogBreeds.filter(breed => { 
-      return breed.charAt(0) === filterbreed
-    })  
-    }
-    listDogs(filterList);
-  }
-
-  // this is to capture the event of someone changing the dropdown selection
-  const opt = document.getElementById('breed-dropdown');
-  opt.addEventListener('change', filterDogs);
 }
 
 // this change the color of the line item clicked
 // e.target.id contains the id of the li clicked
 function changeColor(e){
-  const li = document.getElementById(e.target.id);
-  li.style.color = 'red'
+
+  if (e.target.style.color === 'red') {
+    e.target.style.color = 'black';
+  } else {
+    e.target.style.color = 'red';
+  }
 }
 
+// this function is triiger when a selection is made in the dropdown.
+// it filter out the breed based on selcted value
+// e.target[e.target.selectedIndex].value contains the selected value
+// a blank option was added to html to allow display of all breeds
+function filterDogs(e){
+  e.preventDefault();
+  let filterList = allDogBreeds;
+  const filterbreed = e.target[e.target.selectedIndex].value
+  if (filterbreed !== "all"){
+      filterList = allDogBreeds.filter(breed => { 
+    return breed.charAt(0) === filterbreed
+  })  
+  }
+  listDogs(filterList);
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchData()
-})
+// this function is called to build the item list
+// it clears out the list first anf build a new one based on the list passed
+// each item in list is given an id for event trigger (change color)
+function listDogs(dogs){
+  ul.innerHTML = ""
+  dogs.forEach(brd => {
+    let dog = `<li class="dog-list">${brd}</li> `
+    ul.innerHTML += dog
+  })
+}
+main()
