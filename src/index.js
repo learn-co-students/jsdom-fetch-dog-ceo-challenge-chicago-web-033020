38e0e6da54a06c5ebd642c1232558a0ce3f76e31
swaddell7 <stefani.waddell@hotmail.com>
2020-05-20 12:15:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function() {
+
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const dogImageContainer = document.getElementById('dog-image-container')
+
+  fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(data => console.log(data.message))
+
+})
